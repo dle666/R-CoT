88b7c9c8b0ff499b333a4c2d086fd17d303b7c49
dle666 <107533509+dle666@users.noreply.github.com>
2024-10-20 13:21:36 +0800

    Update rcot2b_finetune_lora.sh

--- R-CoT2B-main/shell/R-CoT-2B/rcot2b_finetune_lora.sh
+++ R-CoT2B-main/shell/R-CoT-2B/rcot2b_finetune_lora.sh
@@ -11,7 +11,7 @@ export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
 export LAUNCHER=pytorch
 
-OUTPUT_DIR='work_dirs/minimonkey_chat_lora'
+OUTPUT_DIR='work_dirs/rcot-2b_lora'
 
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
@@ -34,7 +34,7 @@ torchrun \
   --model_name_or_path "internvl_v2" \
   --conv_style "internlm2-chat" \
   --output_dir ${OUTPUT_DIR} \
-  --meta_path "shell/data/internvl_1_2_finetune.json" \
+  --meta_path "shell/data/rcot_finetune.json" \
   --overwrite_output_dir True \
   --force_image_size 448 \
   --max_dynamic_patch 6 \
@@ -54,7 +54,7 @@ torchrun \
   --save_strategy "steps" \
   --save_steps 200 \
   --save_total_limit 1 \
-  --learning_rate 4e-6 \
+  --learning_rate 4e-5 \
   --weight_decay 0.01 \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
